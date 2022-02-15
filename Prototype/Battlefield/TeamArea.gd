93915728ea2bc4b6cebfff0da7ebb947caba671a
wongselent <david.agung.satrio@gmail.com>
2022-02-16 05:20:40 +0800

    add creep spawn position
    - creep spawn now has 3 lanes: top middle, and bottom
    - remove flip mode in red team

--- Prototype/Battlefield/TeamArea.gd
+++ Prototype/Battlefield/TeamArea.gd
@@ -7,26 +7,29 @@ export(Units.TeamID) var team = Units.TeamID.Blue setget set_team
 export(bool) var mirror_mode = false
 
 var _is_ready: bool = false
-var creep_spawner_position: Vector2 = Vector2.ZERO
+var top_creep_spawner_position: Vector2 = Vector2.ZERO
+var mid_creep_spawner_position: Vector2 = Vector2.ZERO
+var bottom_creep_spawner_position: Vector2 = Vector2.ZERO
 var buildings: Array = []
 
 onready var building_container: YSort = $BuildingContainer
+onready var top_creep_spawner: Position2D = $TopCreepSpawner
+onready var mid_creep_spawner: Position2D = $MidCreepSpawner
+onready var bottom_creep_spawner: Position2D = $BottomCreepSpawner
 
 
 func set_team(value: int) -> void:
 	team = value
 	if _is_ready:
 		setup_team()
-		if team == Units.TeamID.Red:
-			scale.x = -1
-			return
-		scale.x = 1
 
 
 func _ready() -> void:
 	if not Engine.editor_hint:
 		_is_ready = true
-		creep_spawner_position = $CreepSpawnerPosition.global_position
+		top_creep_spawner_position = top_creep_spawner.global_position
+		mid_creep_spawner_position = mid_creep_spawner.global_position
+		bottom_creep_spawner_position = bottom_creep_spawner.global_position
 		buildings = $BuildingContainer.get_children()
 		
 		Game.connect("playing", self, "_on_Game_playing")
