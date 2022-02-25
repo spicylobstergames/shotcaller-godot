a6cb50f0a8656ca206056a5dadc70825e1054e3b
Daniel Wart <dman.wart@gmail.com>
2022-02-25 15:00:51 -0800

    Implemented navigation and collision map generation code based on polygons rather than tiles

--- Prototype/Character/BT/Leaves/MoveTowardsEnemyBaseTower.gd
+++ Prototype/Character/BT/Leaves/MoveTowardsEnemyBaseTower.gd
@@ -7,14 +7,13 @@ var move_points: Array = []
 
 
 func do_stuff(agent: Node) -> int:
-	if move_points.empty():
-		for v in agent.get("move_points"):
-			move_points.append(GSAIUtils.to_vector3(v))
+	move_points = []
+	for v in agent.get("move_points"):
+		move_points.append(GSAIUtils.to_vector3(v))
 	var distance_threshold = agent.get_node("Skills").get_skill(0).get_range() * 0.75
 	if agent.global_position.distance_to(agent.targeted_enemy.global_position) <= distance_threshold:
 		return NodeStatus.Success
-		
-	""" ERROR move_points must have at least 2 points
+	
 	var path = GSAIPath.new(move_points)
 	var ai_follow_path = GSAIFollowPath.new(agent.ai_agent, path)
 	ai_follow_path.path_offset = 0.1
@@ -25,5 +24,5 @@ func do_stuff(agent: Node) -> int:
 	ai_follow_path.is_arrive_enabled = false
 	
 	ai_follow_path.calculate_steering(agent.ai_accel)
-	"""
+
 	return NodeStatus.In_Progress
