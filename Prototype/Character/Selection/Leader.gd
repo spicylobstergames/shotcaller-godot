b91c2bd1723b719b6e8a680037f105d1c8649b5d
@racascou <rafaelcastrocouto@gmail.com>
2022-03-07 15:36:22 -0300

    leader selection

---
+++ Prototype/Character/Selection/Leader.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var curent_leader = "maori"
+
+onready var maori_button = find_node(curent_leader)
+onready var group = maori_button.group
+
+
+func _ready() -> void:
+	Game.connect("playing", self, "_on_Game_playing")
+
+func _on_Game_playing() -> void:
+	hide()
+
+func _on_button_pressed(pressed):
+	var pressed_button = group.get_pressed_button()
+	if pressed and curent_leader != pressed_button.name:		
+		curent_leader = pressed_button.name
+		print(curent_leader)
