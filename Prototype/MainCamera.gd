c6833d1a3fbb36699152219f2df6fb694ac47f41
@racascou <rafaelcastrocouto@gmail.com>
2022-03-10 21:16:02 -0300

    leaders anim, camera zoom 3lvls, camera pan fix

--- Prototype/MainCamera.gd
+++ Prototype/MainCamera.gd
@@ -4,33 +4,31 @@ var is_panning:bool = false
 
 var relative_position:Vector2 = Vector2(0.0,0.0)
 
-var zoom_limit:Vector2 = Vector2(0.2,3.5)
-
+var zoom_limit:Vector2 = Vector2(0.32,3.52)
 var margin:int = limit_right;
 	
 func _unhandled_input(event):
-	
+	# touch
 	if event is InputEventScreenTouch:
 		is_panning = event.is_pressed()
 	elif event is InputEventScreenDrag:
-		if is_panning: relative_position = Vector2(-1 * event.relative)#translate(Vector2(-1 * event.relative))
-		else: relative_position = Vector2(0,0)
-		 
+		if is_panning: relative_position = Vector2(-1 * event.relative)
+	# mouse
 	if event.is_action("pan"):
 		is_panning = event.is_action_pressed("pan")
 	elif event is InputEventMouseMotion:
-		if is_panning: relative_position = Vector2(-1 * event.relative)#translate(Vector2(-1 * event.relative))
-		else: relative_position = Vector2(0,0)
-		
-	var m:float;
+		if is_panning: relative_position = Vector2(-1 * event.relative)
+	# zoom
 	if event.is_action_pressed("zoom_in"):
-		zoom *= 0.9
-		m = max(zoom_limit.x, zoom.x)
-		zoom = Vector2(m,m)
+		if zoom.x == zoom_limit.y:
+			zoom = Vector2(1,1)
+		elif zoom.x == 1:
+			zoom = Vector2(zoom_limit.x,zoom_limit.x)
 	if event.is_action_pressed("zoom_out"):
-		zoom *= 1.1
-		m = min(zoom_limit.y, zoom.x)
-		zoom = Vector2(m,m)
+		if zoom.x == zoom_limit.x:
+			zoom = Vector2(1,1)
+		elif zoom.x == 1:
+			zoom = Vector2(zoom_limit.y,zoom_limit.y)
 
 
 func _process(delta):
@@ -41,7 +39,8 @@ func _process(delta):
 	limit_left = -margin
 	limit_right = margin
 	
-	if is_panning: translate(relative_position)
+	if is_panning: translate(relative_position * zoom.x)
+	relative_position = Vector2(0,0)
 	
 	if ratio >= 1:
 		limit_left = -margin - (margin * (ratio-1) * (zoom.x-zoom_limit[0]) * 0.6)
