3d6c1b665c33c573ecc5141bf5ca733e929f0df0
@racascou <rafaelcastrocouto@gmail.com>
2022-03-14 18:32:41 -0300

    rollo animations

--- Prototype/Character/Child/Leader/Rollo/Rollo.gd
+++ Prototype/Character/Child/Leader/Rollo/Rollo.gd
@@ -1,1 +1,11 @@
 extends "res://Character/Child/Leader/Leader.gd"
+
+func _setup_team() -> void:
+	match team:
+		Units.TeamID.Blue:
+			$TextureContainer/AnimatedSprite.frame = 0
+			$TextureContainer/Weapon.frame = 0
+		Units.TeamID.Red:
+			$TextureContainer/AnimatedSprite.frame = 1
+			$TextureContainer/Weapon.frame = 1
+	._setup_team()
