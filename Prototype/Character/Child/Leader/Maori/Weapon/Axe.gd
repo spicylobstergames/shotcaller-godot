52f51844ad0bb889eb2c3d8dfe95c4458292062a
@racascou <rafaelcastrocouto@gmail.com>
2022-03-15 16:29:45 -0300

    improved stuck projectiles rotation

--- Prototype/Character/Child/Leader/Maori/Weapon/Axe.gd
+++ Prototype/Character/Child/Leader/Maori/Weapon/Axe.gd
@@ -10,8 +10,8 @@ func on_impact(enemy):
 		enemy.get_node("TextureContainer/Sprite").add_child(new_stuck_axe)
 	new_stuck_axe.global_rotation = global_rotation
 	new_stuck_axe.global_position = global_position
-	new_stuck_axe.rotate(rand_range(-0.1,0.1))
-	new_stuck_axe.global_position += Vector2(rand_range(6.0,12.0),0.0).rotated(global_rotation)
+	new_stuck_axe.rotate(rand_range(-0.4,0.4))
+	new_stuck_axe.global_position += Vector2(rand_range(5,10),rand_range(-2,2)).rotated(global_rotation)
 	# I have no idea why the axe y scale is being flipped on those cases
 	# but this fixes it
 	if (enemy.team == Units.TeamID.Blue or enemy.get_node("TextureContainer").scale.x == -1):
