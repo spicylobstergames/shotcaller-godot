866e22fb69c10a8d8c27f520a72572f05b9db6f0
@racascou <rafaelcastrocouto@gmail.com>
2022-04-08 23:34:12 -0300

    replace uppercase

---
+++ Prototype/items/item_button.gd
@@ -0,0 +1,18 @@
+ extends Button
+
+var item = null
+
+
+func setup(new_item):
+	if new_item == null:
+		self.item = null
+		self.name = ""
+		self.icon = null
+		self.hint_tooltip = ""
+		self.disabled = true
+		return
+	
+	self.item = new_item
+	self.name = new_item.name
+	self.icon = new_item.sprite
+	self.hint_tooltip = new_item.description
