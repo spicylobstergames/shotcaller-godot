13a7c96d881d965f84cf681c061d9b394e076ac7
@racascou <rafaelcastrocouto@gmail.com>
2022-10-18 15:07:18 -0300

    fix random leader
    Closes #115

--- prototype/ui/leader_preview/leader_preview.gd
+++ prototype/ui/leader_preview/leader_preview.gd
@@ -8,14 +8,15 @@ func _ready():
 	
 func prepare(leader):
 	empty()
-	var leader_scene = load("res://leaders/%s.tscn" % leader)
-	var leader_instance = leader_scene.instance()
-	$"%leader_name".text = leader
-	for ability in leader_instance.get_node("behavior/abilities").get_children():
-		var ability_preview = ability_preview_scene.instance()
-		ability_preview.prepare(ability.icon, ability.ability_name, ability.description)
-		ability_container.add_child(ability_preview)
-	leader_instance.queue_free()
+	if leader != 'random':
+		var leader_scene = load("res://leaders/%s.tscn" % leader)
+		var leader_instance = leader_scene.instance()
+		$"%leader_name".text = leader
+		for ability in leader_instance.get_node("behavior/abilities").get_children():
+			var ability_preview = ability_preview_scene.instance()
+			ability_preview.prepare(ability.icon, ability.ability_name, ability.description)
+			ability_container.add_child(ability_preview)
+		leader_instance.queue_free()
 
 
 func empty():
