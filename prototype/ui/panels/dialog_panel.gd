568ada4e428fbb2be7c11696e69da8d26bbc26a8
@racascou <rafaelcastrocouto@gmail.com>
2023-02-28 14:36:05 +0100

    hide dialog timer

--- prototype/ui/panels/dialog_panel.gd
+++ prototype/ui/panels/dialog_panel.gd
@@ -10,6 +10,8 @@ onready var msg := $"%msg"
 onready var control_delay := $"%control_delay"
 onready var sprite := $"%sprite"
 
+var can_hide := false
+
 func _ready():
 	game = get_tree().get_current_scene()
 	game.connect("game_started", self, "campaign_start")
@@ -23,11 +25,22 @@ func campaign_start():
 
 func show_msg(leader, msg_text):
 	show()
+	can_hide = false
 	game.selection.select_unit(leader)
 	# animate text
 	msg.text = msg_text
 	#var sprite = index of leader
 	#$panel/portrait/sprite.region_rect.position.x = sprite * 64
 	display_name.text = leader.name
-	#	yield(get_tree().create_timer(2), "timeout")
-	# enable hide
+	yield(get_tree().create_timer(2), "timeout")
+	can_hide = true
+
+
+func hide_msg():
+	if can_hide:
+		hide()
+
+
+func _input(event):
+	if event.is_pressed():
+		game.ui.dialog.hide_msg()
