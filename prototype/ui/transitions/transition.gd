fcd7561b99f6fc9da9a8aeee368ee375ed9e83d4
@racascou <rafaelcastrocouto@gmail.com>
2023-04-11 15:38:07 +0200

    tileset

--- prototype/ui/transitions/transition.gd
+++ prototype/ui/transitions/transition.gd
@@ -7,9 +7,6 @@ signal transition_completed
 
 
 func _ready():
-	#var err = tween.connect("tween_all_completed",Callable(self,"transition_done"))
-	#assert(err == OK)
-	# For testing on its own
 	if get_tree().current_scene == self:
 		start_transition()
 
@@ -18,7 +15,6 @@ func start_transition(backwards = false):
 	show()
 	var start = 0.0 if not backwards else 2.0
 	var end = 2.0 if not backwards else 0.0
-	#tween.interpolate_property(texture_rect.get_material(), "shader_param/progress", start, end, 2, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 	var material = texture_rect.get_material()
 	material.set("shader_param/progress", start)
 	tween.set_ease(Tween.EASE_OUT)
