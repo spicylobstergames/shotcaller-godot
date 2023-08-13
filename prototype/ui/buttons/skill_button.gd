6cecb2ccd63b066ecf3c18cd3d1faaacd917616a
Mounir Tohami <53877170+WhalesState@users.noreply.github.com>
2023-08-13 05:44:56 +0300

    added a pixel theme and changed texture filters

--- prototype/ui/buttons/skill_button.gd
+++ prototype/ui/buttons/skill_button.gd
@@ -31,7 +31,7 @@ func _button_down():
 		return
 	# Apply all skills effects
 	for effect in skill.effects:
-		var result = await effect.call_func(skill.effects, skill.parameters, skill.visualize)
+l		var result = await effect.call_func(skill.effects, skill.parameters, skill.visualize)
 		# if effect wasn't successful used, then we need to abort using skill
 		if !result:
 			self.button_pressed = false
