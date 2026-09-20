7de9b0c188280258802914c9381bdc698087cf7a
@racascou <rafaelcastrocouto@gmail.com>
2026-09-20 17:33:16 +0000

    remove behavior node

--- prototype/ui/menus/leader_preview/leader_preview.gd
+++ prototype/ui/menus/leader_preview/leader_preview.gd
@@ -18,7 +18,7 @@ func prepare(leader):
 		var leader_scene = load("res://unit/leaders/%s.tscn" % leader)
 		var leader_instance = leader_scene.instantiate()
 		leader_name_label.text = Utils.first_to_uppper(leader)
-		for ability in leader_instance.get_node("behavior/abilities").get_children():
+		for ability in leader_instance.get_node("goap_agent/abilities").get_children():
 			var ability_preview = ability_preview_scene.instantiate()
 			ability_preview.prepare(ability.icon, ability.ability_name, ability.description)
 			abilities_preview_container.add_child(ability_preview)
